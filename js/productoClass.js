6197707f658503bd5a19b91f47f2b708a402adc9
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2021-11-15 11:35:31 -0300

    se agrego la pagina producto class, la pagina admin se comporta ahora como modulo y se hicieron las respectivas exportaciones e importaciones

---
+++ js/productoClass.js
@@ -0,0 +1,12 @@
+export class Producto {
+    constructor(campoCodigo, campoProducto, campoDescripcion, campoCantidad, campoURL){
+        this.codigo = campoCodigo;
+        this.producto = campoProducto;
+        this.cantidad = campoCantidad;
+        this.descripcion = campoDescripcion;
+        this.cantidad = campoCantidad;
+        this.url = campoURL;
+    }
+
+    //agregar los get y set
+}
